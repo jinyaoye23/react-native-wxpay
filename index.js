b3d764c80470b79cf6353a33d255da752e910c22
shizhenzhen <kumbaya123@126.com>
2019-09-26 15:27:44 +0800

    android集成微信支付sdk

--- index.js
+++ index.js
@@ -1,29 +1,39 @@
-import { NativeModules } from 'react-native';
+import { NativeModules,Platform } from 'react-native';
 
-const Wxpay = NativeModules.WxpayModule;
+
+const Wxpay = NativeModules.WxpayModuleModule;
+// export default NativeModules.WxpayModuleModule;
 
 export default class WxpayModule {
-  async wxPayAction(wxDic) {
+  static registerApp(appid){
+    Wxpay.registerApp(appid)
+  }
+  static wxPayAction(wxDic) {
     //wxDic为从服务器获取的支付信息
-    Wxpay.isSupported().then((isSupported) => {
-      if (isSupported) {
-        Wxpay.pay(wxDic).then((data) => {
-          console.log('data=' + JSON.stringify(data));
-          //data = {"errCode":0}//支付成功
-          //data={"errCode":-2} //取消支付
-          if (data.errCode == 0) {
-            //支付成功
-
-          } else {
-            //支付失败
-          }
-        });
-      } else {
-        //未安装微信或当前微信版本较低
-      }
-    }).catch((err) => {
-      console.log('err=' + err);
-      this.refs.toast.show('支付失败');
-    });
+    return new Promise((resolve, reject) => {
+      Wxpay.isSupported().then((isSupported) => {
+        if (isSupported) {
+          Wxpay.pay(wxDic).then((data) => {
+            console.log('data=' + JSON.stringify(data));
+            //data = {"errCode":0}//支付成功
+            //data={"errCode":-2} //取消支付
+            if (data.errCode == 0) {
+              //支付成功
+              resolve(data)
+            } else {
+              //支付失败
+              reject(data)
+            }
+          });
+        } else {
+          //未安装微信或当前微信版本较低
+          reject(isSupported)
+        }
+      }).catch((err) => {
+        console.log('err=' + err);
+        // this.refs.toast.show('支付失败');
+        reject(err)
+      });
+    })
   }
 };
